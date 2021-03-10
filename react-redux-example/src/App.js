6adcfd0008bbddfeae9324c4d42456208e72b358
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-10 12:28:07 -0800

    added react redux example demo

---
+++ react-redux-example/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      {isLogged ? (
+        <h3>You got the Secret Information!</h3>
+      ) : (
+        <h3>You didn't get the secret information!</h3>
+      )}
+    </div>
+  );
+}
+
+export default App;
