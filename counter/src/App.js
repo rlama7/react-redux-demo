13b752f3e083e456fc0a8fe4d52cd0719fdcc2b2
Ratna Lama <rlama@ratnalamaCOM.local>
2021-03-20 19:09:14 -0700

    added counter example

---
+++ counter/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Counter Example</p>
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
