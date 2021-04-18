1f3f9888b78567784acfa344885a66ea32092e29
Ratna Lama <rlama@ratnalamaCOM.local>
2021-04-18 16:25:39 -0700

    corrected typo

--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Square = ({ value, onClick }) => {
   const style = value ? `squares ${value}` : `squares`;
   return (
-    <button className={style} s onClick={onClick}>
+    <button className={style} onClick={onClick}>
       {value}
     </button>
   );
