f94fbafadc833d9d51cc03ad4954721039e60240
Ratna Lama <rlama@ratnalamaCOM.local>
2021-04-27 19:42:11 -0700

    added basic search and filter functionality

--- search-app/src/App.js
+++ search-app/src/App.js
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import './App.css';
+import JSONDATA from './data/MOCK_DATA.json';
 
 function App() {
-  const handleChange = (e) => console.log(e.target.value);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (e) => setSearchTerm(e.target.value);
   return (
     <div className="App">
-      <hr />
       <input placeholder="Search..." type="text" onChange={handleChange} />
+      <ul>
+        {JSONDATA.filter((val) => {
+          if (searchTerm === '') {
+            return val;
+          } else if (
+            val.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+          ) {
+            return val;
+          }
+        }).map((person, index) => {
+          return (
+            <li key={person.social_security}>
+              {person.first_name} {person.last_name}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
