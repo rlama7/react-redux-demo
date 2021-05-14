631739c66d5df466b901c7955366dde988bf9634
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-14 09:31:04 -0700

    added fetch example

--- fetch-data/src/App.js
+++ fetch-data/src/App.js
@@ -1,11 +1,32 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // similar to componentDidMount and componentDidUpdate
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url);
+      const data = await response.json();
+      const [item] = data.results;
+      setData(item);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  return { data, loading };
+};
 
 function App() {
   const [num, setNum] = useState(0);
+  const { data, loading } = useFetch('https://api.randomuser.me/');
 
   const increment = () => setNum(num + 1);
   const decrement = () => setNum(num - 1);
+  // const getNewUser = () => setData(data);
 
   return (
     <div className="App">
@@ -13,6 +34,18 @@ function App() {
       <h2>{num}</h2>
       <button onClick={increment}>Increment + </button>
       <button onClick={decrement}>Decrement - </button>
+      <hr />
+
+      {/*<button onClick={getNewUser}>Click Me</button>*/}
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <div>
+          <h3>
+            User: {data.name.first} {data.name.last}
+          </h3>
+        </div>
+      )}
     </div>
   );
 }
