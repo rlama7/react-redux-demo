4e1c7d8ea81031e74aa4f6c071bb4dd5ac131412
Ratna Lama <rlama@ratnalamaCOM.local>
2021-05-14 10:03:20 -0700

    added counter example with hooks

--- fetch-data/src/App.js
+++ fetch-data/src/App.js
@@ -1,51 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const useFetch = (url) => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  // similar to componentDidMount and componentDidUpdate
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(url);
-      const data = await response.json();
-      const [item] = data.results;
-      setData(item);
-      setLoading(false);
-    }
-    fetchData();
-  }, []);
-
-  return { data, loading };
-};
-
 function App() {
-  const [num, setNum] = useState(0);
-  const { data, loading } = useFetch('https://api.randomuser.me/');
+  const [count, setCount] = useState(0);
 
-  const increment = () => setNum(num + 1);
-  const decrement = () => setNum(num - 1);
-  // const getNewUser = () => setData(data);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
 
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <h2>{num}</h2>
-      <button onClick={increment}>Increment + </button>
-      <button onClick={decrement}>Decrement - </button>
-      <hr />
-
-      {/*<button onClick={getNewUser}>Click Me</button>*/}
-      {loading ? (
-        <div>loading...</div>
-      ) : (
-        <div>
-          <h3>
-            User: {data.name.first} {data.name.last}
-          </h3>
-        </div>
-      )}
+      <h2>{count}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
