4db4818429d8c8b8aaa5e64ab851ef9b013a5a02
Ratna Lama <rlama@ratnalamaCOM.local>
2021-06-08 22:10:33 -0700

    added fetch and display data

--- json-ops/public/scripts/script.js
+++ json-ops/public/scripts/script.js
@@ -1,8 +1,38 @@
-const fetchData = async (url) => {
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(data);
+const fetchData = async () => {
+  try {
+    console.log('Initiating fetch...');
+    const res = await fetch(url);
+    const record = await res.json();
+    console.log(record.data);
+
+    const nameList = record.data
+      .map((user) => {
+        return `
+          <div class="user">
+            <p><img src=${user.avatar} alt=${user.first_name} /></p>
+            <p>${user.first_name} ${user.last_name}</p>
+            <p>${user.email}</p>
+            
+            <div class="connect">
+              <button>Connect with Me</button>
+            </div>
+            
+          </div>
+          
+        `;
+      })
+      .join('');
+
+    // console.log(nameList);
+
+    const [email, first_name, last_name] = record.data;
+    console.log(email, first_name, last_name);
+
+    document.getElementById('app').insertAdjacentHTML('afterbegin', nameList);
+  } catch (error) {
+    throw error;
+  }
 };
 
 const url = 'https://reqres.in/api/users?page=1';
-console.log(fetchData(url));
+fetchData(url);
